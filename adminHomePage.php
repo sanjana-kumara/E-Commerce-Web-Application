14afb3bb43dc65d1a6ee0a89456e79f2f475485e
sanjana-kumara <sanjanakumara92@gmail.com>
2024-05-25 00:50:30 +0530

    Design Admin Dashboard 2

--- adminHomePage.php
+++ adminHomePage.php
@@ -15,13 +15,14 @@ if (isset($_SESSION["ad"])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
         <link rel="stylesheet" href="style.css">
-        <title>Document</title>
+        <title>Halgahawatta Hardware Admin Panel</title>
     </head>
 
     <body>
         <div class="container-fluid vh-100 d-flex justify-content-center">
             <div class="row align-content-center">
 
+                <!-- Admin Navbar -->
                 <div>
                     <nav class="navbar bg-primary fixed-top col-12">
 
@@ -30,23 +31,29 @@ if (isset($_SESSION["ad"])) {
                         </button>
                         <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                             <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                                <h5 class="offcanvas-title fs-2 fw-bold" id="offcanvasDarkNavbarLabel">Admin</h5>
                                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div class="offcanvas-body">
                                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                     <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                        <a class="nav-link text-light fs-3" href="#">User Management</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">Link</a>
+                                        <a class="nav-link text-light fs-3" href="#">Product Management</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link text-light fs-3" href="#">Stock Management</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link text-light fs-3" href="#">Oder Management</a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
 
                         <div class="col-5 mt-lg-1 mt-2 d-flex justify-content-lg-center">
-                            <h2 class="fw-bold fs-1" style = "color:yellow;">Welcome Admin</h2>
+                            <h2 class="fw-bold fs-1" style="color:yellow;">Welcome Admin</h2>
                         </div>
 
                         <div class="d-none d-lg-block col-5 col-lg-6 mt-lg-1 mt-2 d-flex" id="search">
@@ -55,11 +62,25 @@ if (isset($_SESSION["ad"])) {
                                 <button class="btn btn btn-outline-warning col-2 me-2 ">Search</button>
                             </form>
                         </div>
-
                     </nav>
+                </div>
+                <!-- Admin Navbar -->
+                <!-- Admin User Management -->
+
+                <div class="row">
+                    <div class="col-6 umanagement">
+                        <div class="text-center fs-2 fw-bold text-light"> Users </div>
+                        <!-- Admin User Management -->
 
 
+                        
+                        <!-- Admin User Management -->
 
+                    </div>
+                    <div class="col-6">
+
+
+                    </div>
                 </div>
 
             </div>
